86ef7c1b735bd5e6d5bcd52ac6d09efb37d8229f
Coulsen Anderson <coulsen.anderson@gmail.com>
2022-08-18 16:56:13 -0600

    changes I made

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -118,22 +118,22 @@ var score = 74
 ////////// PROBLEM 8 //////////
 
 // Do not edit the code below.
-var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
+// var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 // Do not edit the code above.
 
 // Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. If it is, console.log '{number} is divisible by 3.'
 
 //Code Here
-for(let i = 0; i <= listOfNumbers.length; i++){
-  if(listOfNumbers[i] % 3 === 0){
-    console.log(`${i} is divisible by 3`)
-  }
-}
+// for(let i = 0; i <= listOfNumbers.length; i++){
+//   if(listOfNumbers[i] % 3 === 0){
+//     console.log(`${i} is divisible by 3`)
+//   }
+// }
 
 
 ////////// PROBLEM 9 //////////
 // Do not edit the code below.
-var letters = ['A', 'B', 'C', 'D', 'E'];
+// var letters = ['A', 'B', 'C', 'D', 'E'];
 // Do not edit the code above.
 
 /*
@@ -141,6 +141,9 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
+// for(let i = 4; i >= 0; i--){
+//   console.log(letters[i]);
+// }
 
 
 ////////// Advanced Problems //////////
@@ -163,6 +166,31 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
+// switch(letterGrade) {
+//   case 'A':
+//     letterGrade = 'A';
+//     console.log('The student is doing excellently.')
+//     break;
+//   case 'B':
+//     letterGrade = 'B';
+//     console.log('The student is doing well.')
+//     break;
+//   case 'C':
+//     letterGrade = 'C';
+//     console.log('The student is doing alright.')
+//     break;
+//   case 'D':
+//     letterGrade = 'D';
+//     console.log('The student is not doing very well.')
+//     break;
+//   case 'F':
+//     letterGrade = 'F'
+//     console.log('The student is failing.')
+//     break;
+//   default: {
+//     console.log('Not an eligible grade.')
+//   }
+// }
 
 
 
@@ -191,3 +219,14 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
+for(let i = 1; i <= 100; i++){
+  if(i % 5 === 0 && i % 3 === 0){
+    console.log('DevMountain')
+  }else if(i % 3 === 0){
+    console.log('Dev')
+  }else if(i % 5 === 0){
+    console.log('Mountain')
+  }else {
+    console.log(i)
+  }
+}
